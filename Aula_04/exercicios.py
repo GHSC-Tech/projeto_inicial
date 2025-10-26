f0e5054cf54feeea44c802d00aba73a2e93827fd
Guilherme <carlosqa.ch@gmail.com>
2025-10-25 22:09:08 -0300

    Atualizando arquivos

--- Aula_04/exercicios.py
+++ Aula_04/exercicios.py
@@ -1,3 +1,10 @@
+# Exercícios de Listas e Dicionários
+# 1-Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
+# 2-Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
+
+# 3-Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. Imprima cada informação.
+
+
 # Crie um dicionário para armazenar informações de um livro,
 # incluindo título, autor e ano de publicação. Imprima cada informação.
 
@@ -47,4 +54,43 @@ print(lista_de_livros_usando_dict["livro_01"])
 
 
 
- {}
+# 4-Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
+# 5-Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, calcule o preço total da lista de compras.
+
+# Exercícios intermediários e mais avançados
+
+# 6. Eliminação de Duplicatas
+# Objetivo: Dada uma lista de emails, remover todos os duplicados.
+
+# 7. Filtragem de Dados
+# Objetivo: Dada uma lista de idades, filtrar apenas aquelas que são maiores ou iguais a 18.
+
+# 8. Ordenação Personalizada
+# Objetivo: Dada uma lista de dicionários representando pessoas, ordená-las pelo nome.
+
+# 9. Agregação de Dados
+# Objetivo: Dado um conjunto de números, calcular a média.
+
+# 10. Divisão de Dados em Grupos
+# Objetivo: Dada uma lista de valores, dividir em duas listas: uma para valores pares e outra para ímpares.
+
+# Exercícios com Dicionários
+
+# 11. Atualização de Dados
+# Objetivo: Dada uma lista de dicionários representando produtos, atualizar o preço de um produto específico.
+
+# 12. Fusão de Dicionários
+# Objetivo: Dados dois dicionários, fundi-los em um único dicionário.
+
+# 13. Filtragem de Dados em Dicionário
+# Objetivo: Dado um dicionário de estoque de produtos, filtrar aqueles com quantidade maior que 0.
+
+# 14. Extração de Chaves e Valores
+# Objetivo: Dado um dicionário, criar listas separadas para suas chaves e valores.
+
+
+# 15. Contagem de Frequência de Itens
+# Objetivo: Dada uma string, contar a frequência de cada caractere usando um dicionário.
+
+
+ #{}
