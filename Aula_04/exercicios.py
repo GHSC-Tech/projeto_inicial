2f864639e2c30075b2658540b1eff7e5af894a62
Guilherme <carlosqa.ch@gmail.com>
2025-10-29 21:51:09 -0300

    Incluindo novos exericícios.

--- Aula_04/exercicios.py
+++ Aula_04/exercicios.py
@@ -424,3 +424,14 @@ for contar in novo_texto:
     
 print(frequencia)
 
+
+# Exercícios de Funções
+# 16 - Escreva uma função que receba uma lista de números e retorne a soma de todos os números.
+
+# 17 - Crie uma função que receba um número como argumento e retorne True se o número for primo e False caso contrário.
+
+# 18 - Desenvolva uma função que receba uma string como argumento e retorne essa string revertida.
+
+# 19 - Implemente uma função que receba dois argumentos: uma lista de números e um número. A função deve retornar todas as combinações de pares na lista que somem ao número dado.
+
+# 20 - Escreva uma função que receba um dicionário e retorne uma lista de chaves ordenadas
